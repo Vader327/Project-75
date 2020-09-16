0df7e5abdb279f69ad70b23a9af4ba058c787106
Saaheer <saaheer.purav@gmail.com>
2020-09-16 11:35:56 +0530

    Added files via git

--- Screens/WriteStoryScreen.js
+++ Screens/WriteStoryScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard, TouchableHighlight, Alert } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard, TouchableHighlight, Alert, KeyboardAvoidingView, ToastAndroid } from 'react-native';
 import { Header } from 'react-native-elements';
 import * as firebase from 'firebase';
 import db from '../config';
@@ -25,52 +25,66 @@ export default class WriteStoryScreen extends React.Component {
       })
       this.setState({title: '', author: '', story: ""});
 
-      Alert.alert("Thank you for submitting a story!");
+      Platform.OS == "ios"
+      ? Alert.alert("Thank you for submitting a story!")
+      : ToastAndroid.show("Thank you for submitting a story!", ToastAndroid.SHORT);
     }
-    if(this.state.title.trim() === ''){
-      Alert.alert("Please enter a story title!");
+    if(this.state.title.trim() === '' && this.state.author.trim() === '' && this.state.story.trim() === ""){
+      Platform.OS == "ios"
+      ? Alert.alert("Please type something!")
+      : ToastAndroid.show("Please type something!", ToastAndroid.SHORT);
     }
-    if(this.state.author.trim() === ''){
-      Alert.alert("Please enter the author name!");
+    else if(this.state.title.trim() === ''){
+      Platform.OS == "ios"
+      ? Alert.alert("Please enter a story title!")
+      : ToastAndroid.show("Please enter a story title!", ToastAndroid.SHORT);
     }
-    if(this.state.story.trim() === ''){
-      Alert.alert("Please enter your story!");
+    else if(this.state.author.trim() === ''){
+      Platform.OS == "ios"
+      ? Alert.alert("Please enter the author name!")
+      : ToastAndroid.show("Please enter the author name!", ToastAndroid.SHORT);
+    }
+    else if(this.state.story.trim() === ""){
+      Platform.OS == "ios"
+      ? Alert.alert("Please enter your story!")
+      : ToastAndroid.show("Please enter your story!", ToastAndroid.SHORT);
     }
   }
 
   render(){
     return (
-      <View style={{height: '100%'}}>
+      <KeyboardAvoidingView behavior="padding" enabled style={{height: '100%', flex:1}}>
+
         <Header
-          containerStyle={{backgroundColor: '#03b898',}}
-          centerComponent={{
-            text: "Story Hub",
-            style:{
-              color: "#ffffff",
-              fontSize: 24,
-              fontWeight: '600',
-            }}} />
-          <TouchableWithoutFeedback style={{flex: 1}} onPress={Keyboard.dismiss}>
-            <View>
-              <TextInput style={styles.input} placeholder="Story Title"
-              value={this.state.title} onChangeText={(text)=>{this.setState({title: text})}} />
+        containerStyle={{backgroundColor: '#03b898',}}
+        centerComponent={{
+          text: "Story Hub",
+          style:{
+            color: "#ffffff",
+            fontSize: 24,
+            fontWeight: '600',
+        }}} />
 
-              <TextInput style={styles.input} placeholder="Author"
-              value={this.state.author} onChangeText={(text)=>{this.setState({author: text})}} />            
+        <TouchableWithoutFeedback style={{flex: 1}} onPress={Keyboard.dismiss}>
+			<View style={{height:'100%'}}>
+				<TextInput style={styles.input} placeholder="Story Title"
+				value={this.state.title} onChangeText={(text)=>{this.setState({title: text})}} />
 
-              <TextInput style={styles.input} placeholder="Write your story" multiline={true}
-              value={this.state.story} onChangeText={(text)=>{this.setState({story: text})}} />
-            </View>
-          </TouchableWithoutFeedback>
+				<TextInput style={styles.input} placeholder="Author"
+				value={this.state.author} onChangeText={(text)=>{this.setState({author: text})}} />            
 
-          <TouchableHighlight style={styles.submit} underlayColor='#03b898' onPress={this.submitStory}
-          onShowUnderlay={()=>{this.setState({submitFocus: true})}}
-          onHideUnderlay={()=>{this.setState({submitFocus: false})}}>
-            <Text style={this.state.submitFocus ? [styles.submitText, {color: '#ffffff'}] : styles.submitText}>
-              Submit
-            </Text>
-          </TouchableHighlight>
-      </View>
+				<TextInput style={styles.input} placeholder="Write your story" multiline={true}
+				value={this.state.story} onChangeText={(text)=>{this.setState({story: text})}} />         
+			</View>
+        </TouchableWithoutFeedback>		
+
+        <TouchableHighlight style={styles.submit} underlayColor='#03b898' onPress={this.submitStory}
+        onShowUnderlay={()=>{this.setState({submitFocus: true})}}
+        onHideUnderlay={()=>{this.setState({submitFocus: false})}}>
+          <Text style={this.state.submitFocus?[styles.submitText,{color:'#ffffff'}]:styles.submitText}>Submit</Text>
+        </TouchableHighlight>
+
+      </KeyboardAvoidingView>   
     );
   }
 }
